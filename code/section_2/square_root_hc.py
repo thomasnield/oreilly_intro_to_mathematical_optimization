315c0368a7849c2fe248eeb6b1d098cea6b41a3b
Thomas Nield <thomasnield@live.com>
2019-06-04 11:09:42 -0500

    Update square_root_hc.py

--- code/section_2/square_root_hc.py
+++ code/section_2/square_root_hc.py
@@ -6,7 +6,7 @@ value = 11.0
 sqrt_candidate = value / 2.0
 
 for i in range(100000):
-    adjust = np.random.standard_t(3, 1)[0]
+    adjust = np.random.normal()
     new_candidate =  sqrt_candidate + adjust 
     
     if  (value - sqrt_candidate ** 2) ** 2 > (value - new_candidate ** 2) ** 2:
